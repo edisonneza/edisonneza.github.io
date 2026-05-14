57a111f63c1c8ce4b3eecad0b34df1cbfaec32ae
Edison Neza (TME) <edison.neza@external.toyota-europe.com>
2026-05-14 22:37:21 +0200

    updated personal landing page

--- js/main.js
+++ js/main.js
@@ -1,114 +1,117 @@
 (function ($) {
-	"use strict";
-	var nav = $('nav');
-  var navHeight = nav.outerHeight();
-  
-//   $('.navbar-toggler').on('click', function() {
-//     if( ! $('#mainNav').hasClass('navbar-reduce')) {
-//       $('#mainNav').addClass('navbar-reduce');
-//     }
-//   })
+  "use strict";
 
-  // Preloader
+  /* ------------------------------------------------
+     Preloader
+  ------------------------------------------------ */
   $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-	}
-	
-	(function(){
-		var otherSkills = [ '.NET 5', 'ASP.NET MVC', 'ASP.NET Core', 'LINQ', 'Entity Framework', 'Dapper', 'SQL Server', 'MySQL', 'SQLite',
-							'ASP.NET Web API', 'ASP Classic', 'VB', 'Javascript', 'Typescript', 'jQuery', 'Boostrap 3/4', 'Materialize CSS', 'GIT', 'TFS', 'PWA',
-							'Sharepoint Framework', 'Microsoft Power Automate', 'React', 'Wordpress', 'Azure DevOps', 'jsPDF',
-				                        'jspdf-invoice-template', 'RavenDB'];
-		$.each(otherSkills, function(index, item){
-			$('p.lead.skills').append('<a href="https://www.google.com/search?q=' + item + '" target="_blank"><span class="badge badge-info">' + item + '</span></a>');
-		})
-	})();
+    setTimeout(function () {
+      $('#preloader').fadeOut(500, function () { $(this).remove(); });
+    }, 350);
   });
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
+  /* ------------------------------------------------
+     Navbar: scroll class + active link
+  ------------------------------------------------ */
+  function updateNav() {
+    if ($(window).scrollTop() > 60) {
+      $('#mainNav').addClass('scrolled');
     } else {
-      $('.back-to-top').fadeOut('slow');
+      $('#mainNav').removeClass('scrolled');
+    }
+
+    if ($(window).scrollTop() > 280) {
+      $('#backToTop').addClass('show');
+    } else {
+      $('#backToTop').removeClass('show');
+    }
+  }
+
+  $(window).on('scroll', updateNav);
+  updateNav();
+
+  /* ------------------------------------------------
+     Back to top
+  ------------------------------------------------ */
+  $('#backToTop').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 700, 'easeInOutExpo');
+  });
+
+  /* ------------------------------------------------
+     Smooth scroll for all .js-scroll anchor links
+  ------------------------------------------------ */
+  $('a.js-scroll[href*="#"]:not([href="#"])').on('click', function () {
+    if (
+      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+      location.hostname === this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate(
+          { scrollTop: target.offset().top - 75 },
+          820,
+          'easeInOutExpo'
+        );
+        return false;
+      }
     }
   });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
+
+  /* Close mobile menu on nav click */
+  $('.js-scroll').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
   });
 
-	/*--/ Star ScrollTop /--*/
-	$('.scrolltop-mf').on("click", function () {
-		$('html, body').animate({
-			scrollTop: 0
-		}, 1000);
-	});
+  /* ------------------------------------------------
+     Typed.js — hero subtitle
+  ------------------------------------------------ */
+  if ($('.text-slider').length === 1) {
+    var typedStrings = $('.text-slider-items').text();
+    new Typed('.text-slider', {
+      strings:       typedStrings.split(','),
+      typeSpeed:     68,
+      backDelay:     1500,
+      backSpeed:     32,
+      loop:          true,
+      smartBackspace: true
+    });
+  }
 
-	/*--/ Star Counter /--*/
-	$('.counter').counterUp({
-		delay: 15,
-		time: 2000
-	});
+  /* ------------------------------------------------
+     Scroll-reveal: IntersectionObserver for .aos-fade
+  ------------------------------------------------ */
+  var revealObserver = new IntersectionObserver(
+    function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in');
+          revealObserver.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.12, rootMargin: '0px 0px -40px 0px' }
+  );
 
-	/*--/ Star Scrolling nav /--*/
-	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: (target.offset().top - navHeight + 5)
-				}, 1000, "easeInOutExpo");
-				return false;
-			}
-		}
-	});
+  document.querySelectorAll('.aos-fade').forEach(function (el) {
+    revealObserver.observe(el);
+  });
 
-	// Closes responsive menu when a scroll trigger link is clicked
-	$('.js-scroll').on("click", function () {
-		$('.navbar-collapse').collapse('hide');
-	});
-
-	// Activate scrollspy to add active class to navbar items on scroll
-	$('body').scrollspy({
-		target: '#mainNav',
-		offset: navHeight
-	});
-	/*--/ End Scrolling nav /--*/
-
-	/*--/ Navbar Menu Reduce /--*/
-	$(window).trigger('scroll');
-	$(window).on('scroll', function () {
-		var pixels = 50; 
-		var top = 1200;
-		if ($(window).scrollTop() > pixels) {
-			$('.navbar-expand-md').addClass('navbar-reduce');
-			$('.navbar-expand-md').removeClass('navbar-trans');
-		} else {
-			$('.navbar-expand-md').addClass('navbar-trans');
-			$('.navbar-expand-md').removeClass('navbar-reduce');
-		}
-		if ($(window).scrollTop() > top) {
-			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
-		} else {
-			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
-		}
-	});
-
-	/*--/ Star Typed /--*/
-	if ($('.text-slider').length == 1) {
-    var typed_strings = $('.text-slider-items').text();
-		var typed = new Typed('.text-slider', {
-			strings: typed_strings.split(','),
-			typeSpeed: 80,
-			loop: true,
-			backDelay: 1100,
-			backSpeed: 30
-		});
-	}
+  /* ------------------------------------------------
+     Active nav link highlighting on scroll
+  ------------------------------------------------ */
+  $(window).on('scroll.spy', function () {
+    var scrollPos = $(window).scrollTop() + 90;
+    $('section[id], footer[id]').each(function () {
+      var id        = $(this).attr('id');
+      var offsetTop = $(this).offset().top;
+      var height    = $(this).outerHeight();
+      if (scrollPos >= offsetTop && scrollPos < offsetTop + height) {
+        $('.nav-link').removeClass('active');
+        $('.nav-link[href="#' + id + '"]').addClass('active');
+      }
+    });
+  });
 
 })(jQuery);
